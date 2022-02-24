53a72893477910f7f5605854f1e6a530e7afbbed
Swapnodeep Biswas <bonny2610@gmail.com>
2022-02-24 15:12:03 +0530

    Nav, Login And About UI done

--- JS Files/index.js
+++ JS Files/index.js
@@ -1,5 +1,13 @@
+// The list appears on clicking on the hamburger icon
 $(document).ready(function () {
   $("#hamburger").click(function () {
-    $("ul").toggleClass("show");
+    $("ul.nav-menu").toggleClass("show");
   });
 });
+
+// The list disappears on selecting an option
+// $(document).ready(function () {
+//   $(".nav-menu").click(function () {
+//     $("ul.nav-menu").toggleClass("hide");
+//   });
+// });
