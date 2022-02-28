174ddea24cd215d638cb57fb25a8b59945216949
Swapnodeep Biswas <bonny2610@gmail.com>
2022-02-28 23:37:02 +0530

    Completed School and Admin Registration Form UI

---
+++ JS Files/site.js
@@ -0,0 +1,9 @@
+// Going to the School registration page
+function schoolReg() {
+    window.open("/Registration-Pages/school-registration.html", "_self");
+}
+
+// Going to the School registration page
+function adminReg() {
+    window.open("./Registration-Pages/admin-registration.html", "_self");
+}
