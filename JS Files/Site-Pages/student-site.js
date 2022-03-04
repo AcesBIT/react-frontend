668bbabebc7ffaa0be0b26fab41fd7924bfba2ab
Swapnodeep Biswas <bonny2610@gmail.com>
2022-03-04 22:56:02 +0530

    Completed the Student-Site UI

--- JS Files/Site-Pages/student-site.js
+++ JS Files/Site-Pages/student-site.js
@@ -13,3 +13,12 @@ setInterval(() => {
 }, 50);
 
 // Percentage value and stroke-dashoffset in keyframes will be affected by the data
+// If the percentage value is n then
+
+// @keyframes anim {
+//     100% {
+//         stroke-dashoffset: 472 - (n / 10 * 472);
+//     }
+// }
+
+// Eg:- n = 80, stroke-dashoffset: 94.4
